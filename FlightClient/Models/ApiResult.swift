6334a7e0b4130689d4d4e8ccaaf5953d9324afc1
Jose Garcia <lowyke@gmail.com>
2019-03-26 12:04:53 +0100

    Removed FLModel WIP added generic API communication parsing

---
+++ FlightClient/Models/ApiResult.swift
@@ -0,0 +1,9 @@
+//
+//  ApiResult.swift
+//  FlightClient
+//
+//  Created by Jose Garcia on 25/03/2019.
+//  Copyright © 2019 Jose Garcia. All rights reserved.
+//
+
+import Foundation
---
+++ FlightClient/Models/UserInfo.swift
@@ -0,0 +1,9 @@
+//
+//  UserInfo.swift
+//  FlightClient
+//
+//  Created by Jose Garcia on 25/03/2019.
+//  Copyright © 2019 Jose Garcia. All rights reserved.
+//
+
+import Foundation
